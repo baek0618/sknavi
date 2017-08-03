5b032248c1b7c8f86afbfb8d902d6231b7a8440a
kr2748 <kr2748@hanmail.net>
2017-08-03 11:08:05 +0900

    디스패처랑 웹 xml 그리고 DAO 주입 간단 예제

--- src/planner/member/memberDAO.java
+++ src/planner/member/memberDAO.java
@@ -1,5 +1,10 @@
 package planner.member;
 
-public class memberDAO {
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
+public class memberDAO extends SqlSessionDaoSupport{
+	
+	public String findID(String id)  {
+		return getSqlSession().selectOne(id);
+	}
 }
