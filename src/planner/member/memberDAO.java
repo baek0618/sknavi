54cd503d62e659379c721fc00f3d1b7c48c49c17
kr2748 <kr2748@hanmail.net>
2017-08-03 18:20:49 +0900

    commit

--- src/planner/member/memberDAO.java
+++ src/planner/member/memberDAO.java
@@ -1,7 +1,9 @@
 package planner.member;
 
 import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class memberDAO extends SqlSessionDaoSupport{
 	
 	public memberVO getMember(String id)  {
