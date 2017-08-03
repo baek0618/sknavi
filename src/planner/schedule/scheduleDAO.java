91934daa16dc62fd2703e9bce36b2949330a6667
user <user@user-PC>
2017-08-03 20:44:59 +0900

    schedule 수정

--- src/planner/schedule/scheduleDAO.java
+++ src/planner/schedule/scheduleDAO.java
@@ -1,5 +1,21 @@
 package planner.schedule;
 
-public class scheduleDAO {
+import java.util.List;
+
+public interface scheduleDAO {
+	/* C: shedule 생성 */
+	public int addSchedule(scheduleVO schedule);
+
+	/* R: shedule 조회(전체) */
+	public List<scheduleVO> getScheduleList();
+
+	/* R: shedule 조회(id) */
+	public scheduleVO getSchedulebyId(String scheduleId);
+
+	/* U: shedule 수정 */
+	public int updateSchedule(scheduleVO schedule);
+
+	/* D: shedule 삭제 */
+	public int removeSchedule(scheduleVO schedule);
 
 }
--- src/planner/schedule/scheduleVO.java
+++ src/planner/schedule/scheduleVO.java
@@ -1,5 +1,71 @@
 package planner.schedule;
 
+import oracle.sql.DATE;
+
 public class scheduleVO {
+	private int scheduleId;
+	private DATE startDate;
+	private int day;
+	private String subject;
+	private String id;
+
+	public int getScheduleId() {
+		return scheduleId;
+	}
+
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
+	}
+
+	public DATE getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(DATE startDate) {
+		this.startDate = startDate;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public scheduleVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public scheduleVO(int scheduleId, DATE startDate, int day, String subject, String id) {
+		super();
+		this.scheduleId = scheduleId;
+		this.startDate = startDate;
+		this.day = day;
+		this.subject = subject;
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "scheduleVO [scheduleId=" + scheduleId + ", startDate=" + startDate + ", day=" + day + ", subject="
+				+ subject + ", id=" + id + "]";
+	}
 
 }
