0d4c4ecab51cdd5fc2eed8eebd2e19e5b57d868e
kr2748 <kr2748@hanmail.net>
2017-08-03 20:32:28 +0900

    placeVO

--- src/planner/place/placeDAO.java
+++ src/planner/place/placeDAO.java
@@ -1,5 +1,5 @@
 package planner.place;
 
 public class placeDAO {
-
+	
 }
--- src/planner/place/placeVO.java
+++ src/planner/place/placeVO.java
@@ -1,5 +1,71 @@
 package planner.place;
 
 public class placeVO {
+	private Integer placeId;
+	private String placeName;
+	private String placeTel;
+	private String detail;
+	private String placeLon;
+	private String placeLat;
+	
+	public placeVO() {}
 
+	public placeVO(Integer placeId, String placeName, String placeTel, String detail, String placeLon,
+			String placeLat) {
+		this.placeId = placeId;
+		this.placeName = placeName;
+		this.placeTel = placeTel;
+		this.detail = detail;
+		this.placeLon = placeLon;
+		this.placeLat = placeLat;
+	}
+
+	public Integer getPlaceId() {
+		return placeId;
+	}
+
+	public void setPlaceId(Integer placeId) {
+		this.placeId = placeId;
+	}
+
+	public String getPlaceName() {
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+
+	public String getPlaceTel() {
+		return placeTel;
+	}
+
+	public void setPlaceTel(String placeTel) {
+		this.placeTel = placeTel;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getPlaceLon() {
+		return placeLon;
+	}
+
+	public void setPlaceLon(String placeLon) {
+		this.placeLon = placeLon;
+	}
+
+	public String getPlaceLat() {
+		return placeLat;
+	}
+
+	public void setPlaceLat(String placeLat) {
+		this.placeLat = placeLat;
+	}
+	
 }
